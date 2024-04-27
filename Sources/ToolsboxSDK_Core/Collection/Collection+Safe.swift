c82bb493ead8cd11e2284ff2462db3fd8dbae755
Thibaud Lambert <thib.lambert@icloud.com>
2024-04-27 23:24:27 +0200

    [Core] Add documentation for Collection+Safe

--- Sources/ToolsboxSDK_Core/Collection/Collection+Safe.swift
+++ Sources/ToolsboxSDK_Core/Collection/Collection+Safe.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
+/// An extension to provide safe subscripting for collections.
 public extension Collection {
 	
+	/// Safely accesses the element at the specified index.
+	///
+	/// - Parameter index: The index of the element to access.
+	/// - Returns: The element at the specified index if it exists, or `nil` otherwise.
+	///
+	/// This subscript returns the element at the specified index, if it is within the bounds of the collection. If the index is out of bounds, `nil` is returned instead of causing a runtime error.
+	///
+	/// ```swift
+	/// let array = [1, 2, 3]
+	/// let element = array[safe: 1] // Returns 2
+	/// let element = array[safe: 5] // Returns nil
+	/// ```
 	subscript(safe index: Index) -> Element? {
 		indices.contains(index) ? self[index] : nil
 	}
